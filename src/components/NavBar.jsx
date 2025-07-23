20879b3d8403edefa9d4c38b950029c702889900
wishromo <hinasaeed1212@gmail.com>
2025-07-23 15:09:58 +0500

    the logout color changed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -126,7 +126,7 @@ const Navbar = () => {
 
             <button
               onClick={handleLogout}
-              className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 transition-colors duration-200"
+              className="px-4 py-2 bg-red-500 text-black rounded-md hover:bg-red-600 transition-colors duration-200"
             >
               Logout
             </button>
